53e98510502d7e980b8fc88571542f90d11a1150
Tristoung2 <115805242+Tristoung2@users.noreply.github.com>
2024-01-13 23:11:15 +0100

    composant shopHome pour la bienvenue

---
+++ src/views/ShopHome.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>{{ welcomeMessage }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        welcomeMessage: "Bienvenue dans la boutique ! :D",
+      };
+    },
+  };
+  </script>
+  
